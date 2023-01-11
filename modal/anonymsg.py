59be8fbbbd7d87e4d209f5c3ee379c893af2d690
daveads <the.daveads@gmail.com>
2023-01-12 00:04:03 +0100

    user notification

--- modal/anonymsg.py
+++ modal/anonymsg.py
@@ -37,19 +37,21 @@ class anonymsg(discord.ui.Modal):
                 
                 user_msg = await interaction.guild.fetch_member(int(user_id))
 
-                await user_msg.send(f"{self.children[1].value}")
+                #receving user
+                embed_user = discord.Embed(title="Incognito bot `anonymous dm`", description=f"{self.children[1].value}", color=0xFF5733)
+                await user_msg.send(embed=embed_user)
                 print(self.children[1].value)
 
                 #LOGS
                 embed_log = discord.Embed(title=f"anonymous message", description=f"{self.children[1].value} \n\n**Sent By:** {interaction.user} <@{interaction.user.id}> \n\n**Sent To:** {user_msg} <@{user_msg.id}>" , color=discord.Colour(0x2f3136),)
-                embed_log.set_footer(text="Powered by Incognito™")
+                embed_log.set_footer(text="Powered by Incognito")
                 await log_chn.send(embed=embed_log)
 
                 #Notify User
                 # role to stop a user from receving anonymous message
-
-
-
+                await annoymdm_chn.send(user_msg.mention)
+                embed_anno = discord.Embed(title="Incognito bot", description=f"Check Your Dm, You just got an anonymous message", color=0xFF5733)
+                await annoymdm_chn.send(embed=embed_anno)
 
 
         except:
