4e4b036eb1bf3b4aecfc61b33724e25d79767c4b
daveads <the.daveads@gmail.com>
2023-01-11 22:21:27 +0100

    added user_bot_dm

--- btn/confess_btn.py
+++ btn/confess_btn.py
@@ -5,8 +5,14 @@ class ConfessBtn(discord.ui.View):
     def __init__(self) -> None:
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Click to confess", custom_id="confess-btn", style=discord.ButtonStyle.grey, emoji="✅")
+    @discord.ui.button(label="Confess", style=discord.ButtonStyle.grey, emoji="😷", custom_id="confess-btn", row=1)
     async def button_callback(self, button, interaction):
-
+        
         #modal = cfs(title="confession")
         await interaction.response.send_modal(cfs(title="Incognito Confession"))
+
+
+    @discord.ui.button(label="Anonymous Dm", style=discord.ButtonStyle.red, emoji="✍️", custom_id="anony-mous", row=1)
+    async def anonymsg(self, button, interaction):
+
+        await interaction.response.send_message("still work in progress")
