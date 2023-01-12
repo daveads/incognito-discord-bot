ed46be0c5e89b6c4a7a835bca7f7f6f80dab3a24
daveads <the.daveads@gmail.com>
2023-01-12 16:03:23 +0100

    impl disable/enable feature

--- modal/anonymsg.py
+++ modal/anonymsg.py
@@ -13,7 +13,8 @@ class anonymsg(discord.ui.Modal):
         
         log_chn = discord.utils.get(interaction.guild.channels, id=1061274643467599922)
         annoymdm_chn = discord.utils.get(interaction.guild.channels, id=1062857811777822820)
-      
+        enable_roleObj = discord.utils.get(interaction.guild.roles, id=1063101418090803200)
+        
         # user = guild.get_member_named("Example#1234")
         #c = interaction.guild.get_member_named("daveads#6337")
         #print("checking id", c.id)
@@ -27,28 +28,33 @@ class anonymsg(discord.ui.Modal):
             if len(user) == 18:
 
                 #user_obj = await self.bot.fetch_user(user_id)
-                print(int(user_id))
+                #print(int(user_id))
                 
                 user_msg = await interaction.guild.fetch_member(int(user_id))
 
-                #receving user
-                embed_user = discord.Embed(title="Incognito bot `anonymous dm`", description=f"{self.children[1].value}", color=0xFF5733)
-                await user_msg.send(embed=embed_user)
+                if enable_roleObj in user_msg.roles:
 
-                #sender
-                await interaction.response.send_message("sent", delete_after=5)
+                    #receving user
+                    embed_user = discord.Embed(title="Incognito bot `anonymous dm`", description=f"{self.children[1].value}", color=0xFF5733)
+                    await user_msg.send(embed=embed_user)
+
+                    #sender
+                    await interaction.response.send_message("sent", delete_after=5)
 
 
-                #LOGS
-                embed_log = discord.Embed(title=f"anonymous message", description=f"{self.children[1].value} \n\n**Sent By:** {interaction.user} <@{interaction.user.id}> \n\n**Sent To:** {user_msg} <@{user_msg.id}>" , color=discord.Colour(0x2f3136),)
-                embed_log.set_footer(text="Powered by Incognito")
-                await log_chn.send(embed=embed_log)
+                    #LOGS
+                    embed_log = discord.Embed(title=f"anonymous message", description=f"{self.children[1].value} \n\n**Sent By:** {interaction.user} <@{interaction.user.id}> \n\n**Sent To:** {user_msg} <@{user_msg.id}>" , color=discord.Colour(0x2f3136),)
+                    embed_log.set_footer(text="Powered by Incognito")
+                    await log_chn.send(embed=embed_log)
 
-                #Notify User
-                # role to stop a user from receving anonymous message
-                await annoymdm_chn.send(user_msg.mention)
-                embed_anno = discord.Embed(title="Incognito bot", description=f"Check Your Dm, You just got an anonymous message", color=0xFF5733)
-                await annoymdm_chn.send(embed=embed_anno)
+                    #Notify User
+                    # role to stop a user from receving anonymous message
+                    await annoymdm_chn.send(user_msg.mention)
+                    embed_anno = discord.Embed(title="Incognito bot", description=f"Check Your Dm, You just got an anonymous message", color=0xFF5733)
+                    await annoymdm_chn.send(embed=embed_anno)
+
+                else: 
+                    await interaction.response.send_message("*You can't send this user a Message* \n **User disabled** `anonymous message`", delete_after=5)
 
                 
 
