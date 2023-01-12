5cd027bbf2f336bc9a3bbaf2eacbe93415f0ecc8
daveads <the.daveads@gmail.com>
2023-01-12 16:03:45 +0100

    changes

--- cogs/enable.py
+++ cogs/enable.py
@@ -11,15 +11,15 @@ class Enable(commands.Cog):
      @commands.slash_command(name = "enable", description = "allows you to receive Anonymous message")
      async def enable(self, ctx: discord.ApplicationContext):
 
-        roleObj = discord.utils.get(ctx.guild.roles, id=1063101418090803200)
+        enable_roleObj = discord.utils.get(ctx.guild.roles, id=1063101418090803200)
         
 
-        if roleObj in ctx.user.roles:
+        if enable_roleObj in ctx.user.roles:
             await ctx.respond("**Anonymous Message** `Already Enabled`", delete_after=5)
 
         else:
             user = await ctx.guild.fetch_member(ctx.author.id)
-            await user.add_roles(roleObj)
+            await user.add_roles(enable_roleObj)
             await ctx.respond("**Anonymous Message** `Enable`", delete_after=5)
 
 def setup(bot):
