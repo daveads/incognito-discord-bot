f2aee4c2d63989bbc61a88381adb52331b261123
daveads <the.daveads@gmail.com>
2023-01-13 01:14:36 +0100

    config parser

--- modal/announce_modal.py
+++ modal/announce_modal.py
@@ -1,5 +1,8 @@
 import discord
 
+from core.config_parser import BotConfigs
+botconfig = BotConfigs()
+
 class announce(discord.ui.Modal):
      def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -13,12 +16,12 @@ class announce(discord.ui.Modal):
 
      async def callback(self, interaction: discord.Interaction):
         
-        announce_chn = discord.utils.get(interaction.guild.channels, id=1062594071278919721)
-        
+        announce_chn = discord.utils.get(interaction.guild.channels, id=botconfig.channel("announcement_chnn"))
+
         embed = discord.Embed(title="Incognito feature annoucement", description=f"Title : `{self.children[0].value}` " , color=discord.Colour(0x2f3136),)
 
         embed.add_field(name="INFO", value=self.children[1].value)
         embed.set_footer(text="From @daveads")
 
         await announce_chn.send(embed=embed)
-        await interaction.response.defer()
+        await interaction.response.send_message("**Announcement Sent!**", delete_after=5)
